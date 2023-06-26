c6cdcce7d4061a1d0e764dc975cf59340fefb11e
AnnitaM <122459719+AnnitaM@users.noreply.github.com>
2023-06-26 20:22:20 +0300

    Add files via upload

---
+++ Hw5.java
@@ -0,0 +1,44 @@
+/*
+Реализуйте структуру телефонной книги с помощью HashMap.
+Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с
+разными телефонами, их необходимо считать, как одного человека с разными телефонами.
+Вывод должен быть отсортирован по убыванию числа телефонов.
+*/
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hw5{
+
+    public static void addNumber(String key, int value, Map<String, ArrayList<Integer>> map){
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+        } else {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(value);
+            map.put(key, list);
+        }
+
+    }
+
+    public static void printBook(Map<String, ArrayList<Integer>> map){
+        for (var item : map.entrySet()) {
+            String phones = "";
+            for(int el: item.getValue()){
+                phones = phones + el + ", ";
+            }
+            System.out.printf("%s: %s \n", item.getKey(), phones);
+        }
+    }
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> bookPhone = new HashMap<>();
+        addNumber("Tamm", 147, bookPhone);
+        addNumber("Tamm", 741, bookPhone);
+        addNumber("Sarapuu", 258, bookPhone);
+        addNumber("Lepp", 963, bookPhone);
+        addNumber("Lepp", 369, bookPhone);
+        addNumber("Saar", 987, bookPhone);
+        printBook(bookPhone);
+    }
+}
